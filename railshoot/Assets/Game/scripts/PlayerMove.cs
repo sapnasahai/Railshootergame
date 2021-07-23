dcb1d12958dfc469ad770faa94fc09ff802d2417
sapnasahai <47326414+sapnasahai@users.noreply.github.com>
2021-07-23 23:17:12 +0530

    camera Setup

---
+++ railshoot/Assets/Game/scripts/PlayerMove.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PathCreation;
+
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] PathCreator path;
+    [SerializeField] EndOfPathInstruction endOfPath;
+    [SerializeField] float speed = 3f;
+    [SerializeField] bool isMoving = true;
+
+
+    [Header("Debug options")]
+    [SerializeField] float previewDistance = 0f;
+    [SerializeField] bool enableDebug;
+ 
+    private float distanceTravelled;
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(path != null && isMoving)
+        {
+
+            distanceTravelled = distanceTravelled + speed * Time.deltaTime;
+            transform.position = path.path.GetPointAtDistance(distanceTravelled, endOfPath);
+            transform.rotation = path.path.GetRotationAtDistance(distanceTravelled, endOfPath);
+        }
+
+
+    }
+
+    private void OnValidate()
+    {
+        transform.position = path.path.GetPointAtDistance(previewDistance, endOfPath);
+        transform.rotation = path.path.GetRotationAtDistance(previewDistance, endOfPath);
+    }
+
+
+}
+
+
